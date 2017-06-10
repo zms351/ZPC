6f965eb00e49c52d108dc74b6afc985275c6299c
zms <zhangmeisheng@gmail.com>
2017-06-10 14:53:36 +0800

    instru parse ok

--- src/com/zms/zpc/emulator/assembler/Assembler.java
+++ src/com/zms/zpc/emulator/assembler/Assembler.java
@@ -64,6 +64,7 @@ public class Assembler {
 
     public static void main(String[] args) throws Exception {
         System.out.println("test");
+        System.out.println(InstruData.Flags);
     }
 
 }
--- src/com/zms/zpc/emulator/assembler/InstruData.java
+++ src/com/zms/zpc/emulator/assembler/InstruData.java
@@ -1,5 +1,7 @@
 package com.zms.zpc.emulator.assembler;
 
+import java.util.*;
+
 /**
  * Created by 张小美 on 17/六月/10.
  * Copyright 2002-2016
@@ -7,6 +9,11 @@ package com.zms.zpc.emulator.assembler;
 public class InstruData {
 
     private String key;
+    private String param;
+    private String code;
+    private String flag;
+
+    public final static Collection<String> Flags=new LinkedHashSet<>();
 
     public InstruData(String line) {
         parse(line);
@@ -15,19 +22,55 @@ public class InstruData {
     protected void parse(String line) {
         line=line.replaceAll("\\s+"," ").trim();
         assert line.length()>0;
-        char[] cs=line.toCharArray();
-        for(int i=0;i<cs.length;i++) {
-            if(!((cs[i]>='A' && cs[i]<='Z') || cs[i]=='_' || (cs[i]>='0' && cs[i]<='9') || (cs[i]>='a' && cs[i]<='z'))) {
-                assert i>0;
-                assert cs[i]==' ';
-                key=line.substring(0,i);
-                break;
+        {
+            char[] cs = line.toCharArray();
+            for (int i = 0; i < cs.length; i++) {
+                if (!((cs[i] >= 'A' && cs[i] <= 'Z') || cs[i] == '_' || (cs[i] >= '0' && cs[i] <= '9') || (cs[i] >= 'a' && cs[i] <= 'z'))) {
+                    assert i > 0;
+                    assert cs[i] == ' ';
+                    key = line.substring(0, i);
+                    break;
+                }
+            }
+        }
+        {
+            line = line.substring(key.length() + 1);
+            int index = line.lastIndexOf(' ');
+            assert index > 0;
+            String flag = line.substring(index + 1).trim();
+            line=line.substring(0,index).trim();
+            this.flag=flag;
+            String[] flags = flag.split(",");
+            assert flags.length>0;
+            for (String s : flags) {
+                if(s.equals("ignore")) {
+                    continue;
+                }
+                assert s.length()>0;
+                String s1=s.toUpperCase();
+                assert s1.equals(s);
+                synchronized (Flags) {
+                    Flags.add(s);
+                }
+            }
+        }
+        {
+            int index=line.indexOf(' ');
+            assert index>0;
+            param=line.substring(0,index).trim();
+            code=line.substring(index+1).trim();
+            if(!"ignore".equals(code)) {
+                assert code.startsWith("[") && code.endsWith("]");
             }
         }
     }
 
     public String getKey() {
         return key;
+    }
+
+    public String getFlag() {
+        return flag;
     }
 
 }
