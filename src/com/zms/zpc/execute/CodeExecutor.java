2bc46a4544a360975d95aa44d82ba7708621eb83
zms <zhangmeisheng@gmail.com>
2017-07-02 17:44:16 +0800

    bug fix

--- src/com/zms/zpc/execute/CodeExecutor.java
+++ src/com/zms/zpc/execute/CodeExecutor.java
@@ -21,14 +21,13 @@ public class CodeExecutor {
     }
 
     protected void pre(PC pc) {
-        Regs regs = pc.cpu.regs;
+        regs = pc.cpu.regs;
         if (regs.bits.pe.get()) {
             throw new NotImplException();
         } else {
             bits = 16;
         }
         instruction.mrs.reg8=false;
-        regs = pc.cpu.regs;
         instruction.bits = regs.bits;
         instruction.bits.status=0;
     }
